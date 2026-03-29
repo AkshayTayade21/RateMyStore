d6a934a9b03d488a84695a5f520812b15173bacb
AkshayTayade21 <tayadeakshay677@gmail.com>
2026-03-29 12:26:57 +0530

    Updated database config for neon

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,23 +1,22 @@
-import {Pool} from 'pg';
+import { Pool } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-const connectToDatabase = async ()=>{
-     try {
+const connectToDatabase = async () => {
+  try {
     await pool.query("SELECT 1");
     console.log("Database connected");
   } catch (err) {
     console.log("Database error", err);
   }
-}
+};
 
 export default connectToDatabase;
